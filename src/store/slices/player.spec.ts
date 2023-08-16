06aa5e857c7ca721d82a2bba79f47c824deacacb
Ruteski <lruteski@gmail.com>
2023-08-16 18:54:41 -0300

    criando API de cursos (JSON Server)

--- src/store/slices/player.spec.ts
+++ src/store/slices/player.spec.ts
@@ -1,5 +1,10 @@
 import {describe, expect, it} from 'vitest'
-import {next, play, player as playerReducer, playerSlice} from './player.ts'
+import {
+  next,
+  play,
+  player as playerReducer,
+  // playerSlice
+} from './player.ts'
 
 // test('reducers', () => {
 //   let state;
