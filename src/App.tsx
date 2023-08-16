5228ec1c793f55c317f57d56776250b941eb7691
Ruteski <lruteski@gmail.com>
2023-08-16 11:50:25 -0300

    configuracao inicial do projeto, instalado e configurado o redux, criado o selector para os todos

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Provider as ReduxProvider } from "react-redux";
+
+import {TodoList} from "./components/TodoList.tsx";
+import {AddTodo} from "./components/AddTodo.tsx";
+import {store} from "./store";
+
+export function App() {
+
+  return (
+    <ReduxProvider store={store}>
+      <TodoList />
+      <AddTodo />
+    </ReduxProvider>
+  )
+}
